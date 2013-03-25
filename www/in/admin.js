05215df2a6ebfd20aa060653ee69886091853ed5
Nathan Woods <big.nate.w@gmail.com>
2013-03-25 12:52:00 -0600

    adding screenManager and functions, just started/work remains

--- www/in/admin.js
+++ www/in/admin.js
@@ -4,6 +4,7 @@ var socket;
 
 $(document).ready(function() {
 	socket = io.connect('/admin');
+	ScreenManager.initManager('screenLayout');
 	
 	socket.on('connect', function() {
 		console.log('socket connected');
@@ -13,18 +14,60 @@ $(document).ready(function() {
 	});
 	socket.on('resizeFrame', function(obj) {
 		console.log(obj);
+		ScreenManager.resizeScreen(obj);
 		// note: can add new screens too
 	});
 	socket.on('loadScreens', function(screens) {
 		console.log(screens);
+		for (var i in screens) {
+			ScreenManager.resizeScreen(screens[i]);
+		}
+		// loop through and add screens
 	});
 	socket.on('leaveScreen', function(id) {
 		console.log('Screen left :' + id);
+		ScreenManager.leaveScreen(id);
 	});
 });
 
 Admin = {
 	'setState': function(state) {
 		socket.emit('setState', state);
+	}
+};
+
+ScreenManager = {
+	'screens': {},
+	'initManager': function(obj) {
+		this.obj = $('#' + obj);
+		this.R = Raphael(obj, this.obj.width(), this.obj.height());
+	},
+	'resizeScreen': function(obj) {
+		if (this.screens[obj.id] == undefined) {
+			var sizeX = 50, sizeY = 50,
+				newX = Math.floor(Math.random()*(this.obj.width()-sizeX)),
+				newY = Math.floor(Math.random()*(this.obj.height()-sizeY));
+			obj.c = this.R.rect(newX,newY,sizeX, sizeY).attr({
+				fill: '#555',
+				stroke: 'none'
+			});
+			obj.c.drag(function(dx, dy) { // move
+				this.attr({x: this.ox + dx, y: this.oy + dy});
+			}, function() { // start
+				this.ox = this.attr('x');
+				this.oy = this.attr('y');
+				this.attr({opacity: 0.5});
+			}, function() { // stop
+				this.attr({opacity: 1});
+			});
+			
+		}
+		this.screens[obj.id] = obj;
+	},
+	'leaveScreen': function(id) {
+		if (this.screens[id] !== undefined) {
+			this.screens[id].c.remove();
+			delete this.screens[id];
+		}
 	}
 };
