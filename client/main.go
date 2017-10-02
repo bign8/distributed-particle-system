5bd41900247fc2509191e5e28435e5e6810e3ab9
Nate Woods <big.nate.w@gmail.com>
2017-10-01 22:44:28 -0600

    Adding resize callback

--- client/main.go
+++ client/main.go
@@ -2,7 +2,16 @@ package main
 
 import "github.com/gopherjs/gopherjs/js"
 
+var canvas = js.Global.Get("document").Call("getElementById", "canvas")
+
+func resize(fn *js.Object) {
+	print("rezising shiz")
+}
+
 func main() {
+	js.Global.Get("window").Call("addEventListener", "resize", resize, false)
+	resize(nil)
+
 	// js.Global.Get("document").Call("write", "Hello world!")
 	js.Global.Set("dps", map[string]interface{}{
 		"test": func() int {
--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 module.exports = config = {
-	
+
 	// System Configuration
 	"system_name"   : "CS-389 Distributed Particle System",
 	"system_server" : "*",
-	"system_port"   : 83,
-	
+	"system_port"   : 8383,
+
 	// Misc Configuration
 	"is_production" : true
 };
