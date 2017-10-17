9db6a36ee28e06e5720c339bf353274b8fc68232
Nate Woods <big.nate.w@gmail.com>
2017-10-17 00:29:10 -0600

    Adding RPC call out logic over pub-sub

--- shared/sock/sock.go
+++ shared/sock/sock.go
@@ -1,6 +1,19 @@
 // Package sock is the websoket package you have all been waiting for!
 package sock
 
+import (
+	"errors"
+	"time"
+)
+
+// Context mirrors context.Context (but with fewer imports)
+type Context interface {
+	Deadline() (deadline time.Time, ok bool)
+	Done() <-chan struct{}
+	Err() error
+	Value(key interface{}) interface{}
+}
+
 // Websocket is the core communication interface we will communicate over
 type Websocket interface {
 	OnClose(func(interface{}))
@@ -20,27 +33,68 @@ func New(addr string, builder Constructor) *Sock {
 type Sock struct {
 }
 
+func (s *Sock) genID() string {
+	return "TODO: random chanel ID generation"
+}
+
 // RPC executes an RPC
-func (s *Sock) RPC(name string, args interface{}) (interface{}, error) {
-	return nil, nil
+func (s *Sock) RPC(ctx Context, name string, data []byte) ([]byte, error) {
+	resc := make(chan []byte, 1)
+	defer close(resc) // yay memory leaks
+
+	// Generate response subscription
+	reply := "rpc.reply_" + s.genID()
+	sub, err := s.Subscribe(reply, func(res []byte) {
+		resc <- res
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer sub.Unsubscribe() // TODO: log error here
+
+	// TODO: add reply name to request
+
+	// Publish request to the cloud
+	if err := s.Publish("rpc."+name, data); err != nil {
+		return nil, err
+	}
+
+	// Pick real deadline
+	after := time.Minute
+	deadline, ok := ctx.Deadline()
+	if ok {
+		after = deadline.Sub(time.Now())
+	}
+
+	// Whichever comes first
+	select {
+	case bits := <-resc:
+		return bits, nil
+	case <-time.After(after):
+		return nil, errors.New("Request Timeout")
+	}
 }
 
 // HostRPC provides a response to a fn
-func (s *Sock) HostRPC(name string, fn func(interface{}) (interface{}, error)) {
-
-}
-
-type Stream struct {
-}
-
-func (s *Sock) Subscribe(name string) (*Stream, error) {
+func (s *Sock) HostRPC(name string, fn func(Context, []byte) ([]byte, error)) (*Stream, error) {
 	return nil, nil
 }
 
+// Stream lets you listen to multiple messages on a socket
+type Stream struct {
+}
+
+// Subscribe to a given broadcast channel
+func (s *Sock) Subscribe(name string, cb func([]byte)) (*Stream, error) {
+	return nil, nil
+}
+
+// Unsubscribe from a process stream
 func (s *Stream) Unsubscribe() error {
 	return nil
 }
 
-func (s *Sock) Publish(name string, data interface{}) error {
+// Publish some data!
+func (s *Sock) Publish(name string, data []byte) error {
 	return nil
 }
