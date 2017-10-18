eff489d086d33bc189b6248ef8741bfa92eee197
Nate Woods <big.nate.w@gmail.com>
2017-10-17 23:54:08 -0600

    Adding more reconnect logic from old PRs

--- shared/sock/sock.go
+++ shared/sock/sock.go
@@ -1,14 +1,23 @@
-// Package sock is the websoket package you have all been waiting for!
+// Package sock is the communication package you have all been waiting for!
 package sock
 
 import (
 	"errors"
+	"math/rand"
 	"time"
 )
 
 const (
 	rpcReqPrefix = "rpc.req."
 	rpcResPrefix = "rpc.res."
+
+	retryCap  = 60 * 1000 // one-minute max retry
+	retryBase = 50        // 50ms base retrys
+)
+
+// Common package errors
+var (
+	ErrClosed = errors.New("CLOSED")
 )
 
 // Context mirrors context.Context (but with fewer imports)
@@ -21,9 +30,8 @@ type Context interface {
 
 // Connection is the core communication interface we will communicate over
 type Connection interface {
-	OnClose(func())
-	OnData(func([]byte))
-	Send([]byte) error
+	Bind(onChange func(isOpen bool), onData func(subject string, data []byte)) error
+	Send(string, []byte) error
 	Close() error
 }
 
@@ -31,15 +39,58 @@ type Connection interface {
 type Constructor func(addr string) (Connection, error)
 
 // New builds a new Sock that performs automatic retires on connection failures
-func New(addr string, builder Constructor) *Sock {
-	// TODO: fun connection logic
-	return &Sock{}
+func New(addr string, builder Constructor) (*Sock, error) {
+	sock := &Sock{
+		build: builder,
+		addr:  addr,
+		err:   ErrClosed,
+	}
+	return sock, sock.start()
 }
 
 // Sock is a wrapper for all RPC and pub-sub style communication
 type Sock struct {
 	Conn      Connection
 	Listeners map[string]func([]byte)
+
+	build Constructor // How to construct a new Connection
+	addr  string      // address of connection
+	att   int         // retry attempt
+	err   error       // active error
+}
+
+// retryDelay gives the current retry delay in milliseconds
+// TODO: https://www.awsarchitectureblog.com/2015/03/backoff.html
+// TODO: benchmark
+func retryDelay(attempt int) (delay, next int) {
+	delay = 1 << uint(attempt-1) // 2^retryAttp
+	delay *= retryBase
+	if delay > retryCap {
+		delay = retryCap
+	}
+	return int(rand.Intn(delay)), attempt + 1
+}
+
+func (s *Sock) start() error {
+	s.Conn, s.err = s.build(s.addr)
+	if s.err != nil {
+		return s.err
+	}
+
+	return s.Conn.Bind(func(isOpen bool) {
+		if isOpen {
+			s.att = 0
+			s.err = nil
+		} else {
+			s.start() // TODO: trap error
+		}
+	}, func(subject string, data []byte) {
+		fn, ok := s.Listeners[subject]
+		if !ok {
+			print("Unsupported function:" + subject)
+		}
+		fn(data)
+	})
 }
 
 func (s *Sock) genID() string {
@@ -48,6 +99,9 @@ func (s *Sock) genID() string {
 
 // RPC executes an RPC
 func (s *Sock) RPC(ctx Context, name string, data []byte) ([]byte, error) {
+	if s.err != nil || s.Conn == nil {
+		return nil, s.err
+	}
 	resc := make(chan []byte, 1)
 	defer close(resc) // yay memory leaks
 
@@ -87,7 +141,10 @@ func (s *Sock) RPC(ctx Context, name string, data []byte) ([]byte, error) {
 }
 
 // HostRPC provides a response to a fn
-func (s *Sock) HostRPC(name string, fn func(Context, []byte) ([]byte, error)) (*Stream, error) {
+func (s *Sock) HostRPC(name string, fn func(Context, []byte) ([]byte, error)) (Stream, error) {
+	if s.err != nil || s.Conn == nil {
+		return nil, s.err
+	}
 	return s.Subscribe(rpcReqPrefix+name, func(data []byte) {
 		id := string(data[:8])
 		res, err := fn(nil, data[8:]) // TODO: pass in context
@@ -101,41 +158,36 @@ func (s *Sock) HostRPC(name string, fn func(Context, []byte) ([]byte, error)) (*
 }
 
 // Stream lets you listen to multiple messages on a socket
-type Stream struct {
-	kill func() error
-}
-
-// Subscribe to a given broadcast channel
-func (s *Sock) Subscribe(name string, cb func([]byte)) (*Stream, error) {
-	checkPubSubName(name)
-	s.Listeners[name] = cb
-	return &Stream{
-		kill: func() error {
-			delete(s.Listeners, name)
-			return nil
-		},
-	}, nil
-}
+type Stream func() error
 
 // Unsubscribe from a process stream
-func (s *Stream) Unsubscribe() (err error) {
-	if s.kill != nil {
-		err = s.kill()
-		s.kill = nil
+func (s Stream) Unsubscribe() error { return s() }
+
+// Subscribe to a given broadcast channel
+func (s *Sock) Subscribe(name string, cb func([]byte)) (Stream, error) {
+	if s.err != nil || s.Conn == nil {
+		return nil, s.err
 	}
-	return err
+	s.Listeners[name] = cb
+	return func() error {
+		delete(s.Listeners, name)
+		return nil
+	}, nil
 }
 
 // Publish some data!
 func (s *Sock) Publish(name string, data []byte) error {
-	checkPubSubName(name)
-	v := len(name) // Thanks binary.LittleEndian
-	message := append([]byte{byte(v), byte(v >> 8)}, data...)
-	return s.Conn.Send(message)
+	if s.err != nil || s.Conn == nil {
+		return s.err
+	}
+	// checkPubSubName(name)
+	// v := len(name) // Thanks binary.LittleEndian
+	// message := append([]byte{byte(v), byte(v >> 8)}, data...)
+	return s.Conn.Send(name, data)
 }
 
-func checkPubSubName(name string) {
-	if len(name) > 16 {
-		panic("Publish name too long: '" + name + "' > 16")
-	}
-}
+// func checkPubSubName(name string) {
+// 	if len(name) > 16 {
+// 		panic("Publish name too long: '" + name + "' > 16")
+// 	}
+// }
